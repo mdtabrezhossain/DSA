0396b045720d1230c591126f367b305714e663cc
mdtabrezhossain <142249992+mdtabrezhossain@users.noreply.github.com>
2026-05-11 23:38:36 +0530

    11-May-2026

--- Stacks/ValidParentheses.java
+++ Stacks/ValidParentheses.java
@@ -1,7 +1,29 @@
-import java.util.Stack;
-
 class ValidParentheses {
-    static boolean isValid(String string) {
+    boolean isValid(String s) {
+        StringBuilder temp = new StringBuilder(s);
+        boolean isPairRemoved = true;
+
+        while (isPairRemoved) {
+            isPairRemoved = false;
+
+            for (int i = 0; i < temp.length() - 1; i++) {
+                char c1 = temp.charAt(i);
+                char c2 = temp.charAt(i + 1);
+
+                if ((c1 == '(' && c2 == ')')
+                        || (c1 == '[' && c2 == ']')
+                        || (c1 == '{' && c2 == '}')) {
+                    temp.delete(i, i + 2);
+                    isPairRemoved = true;
+                    break;
+                }
+            }
+        }
+
+        return temp.length() == 0;
+    }
+
+    boolean isValid2(String string) {
         Stack<Character> stack = new Stack<>();
 
         for (char c : string.toCharArray()) {
