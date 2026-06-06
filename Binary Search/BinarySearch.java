fd01d338dc9e974a328345e23857a85a7a94e028
mdtabrezhossain <142249992+mdtabrezhossain@users.noreply.github.com>
2026-06-06 22:47:37 +0530

    6-Jun-2026

--- Binary Search/BinarySearch.java
+++ Binary Search/BinarySearch.java
@@ -1,27 +1,20 @@
 class BinarySearch {
-    public static void main(String[] args) {
-        int[] numbers = { 1, 3, 5, 7 };
-        int targetElement = 5;
-        int startIdx = 0;
-        int endIdx = numbers.length - 1;
-        boolean flag = false;
+    int search(int[] array, int target) {
+        int start = 0;
+        int end = array.length - 1;
 
-        while (startIdx <= endIdx) {
-            int midIdx = startIdx + (endIdx - startIdx) / 2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-            if (numbers[midIdx] == targetElement) {
-                System.out.println(midIdx);
-                flag = true;
-                break;
-            } else if (targetElement < numbers[midIdx]) {
-                endIdx = midIdx - 1;
-            } else if (targetElement > numbers[midIdx]) {
-                startIdx = midIdx + 1;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
 
-        if (flag == false) {
-            System.out.println("not found");
-        }
+        return -1;
     }
 }
--- Binary Search/SearchIn2DMatrix.java
+++ Binary Search/SearchIn2DMatrix.java
@@ -1,63 +1,37 @@
 class SearchIn2DMatrix {
-    static boolean searchMatrix(int[][] matrix, int target) {
-        int low = 0;
-        int high = matrix.length - 1;
-        int row = -1;
+    boolean searchMatrix(int[][] matrix, int target) {
+        int isFound = -1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        for (int[] row : matrix) {
+            isFound = search(row, target);
 
-            if (matrix[mid][0] == target) {
+            if (isFound != -1) {
                 return true;
             }
-
-            else if (matrix[mid][0] < target) {
-                row = mid;
-                low = mid + 1;
-            }
-
-            else if (matrix[mid][0] > target) {
-                high = mid - 1;
-            }
-
         }
+
+        return isFound != -1;
+    }
+
+    boolean searchMatrix2(int[][] matrix, int target) {
+        int row = findRow(matrix, target);
 
         if (row == -1) {
             return false;
         }
 
-        low = 0;
-        high = matrix[row].length - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (matrix[row][mid] == target) {
-                return true;
-            }
-
-            else if (matrix[row][mid] < target) {
-                low = mid + 1;
-            }
-
-            else if (matrix[row][mid] > target) {
-                high = mid - 1;
-            }
-
-        }
-
-        return false;
+        return searchInRow(matrix[row], target);
     }
 
-    static boolean searchMatrix2(int[][] matrix, int target) {
+    static boolean searchMatrix3(int[][] matrix, int target) {
         int rows = matrix.length;
         int cols = matrix[0].length;
 
-        int low = 0;
-        int high = rows * cols - 1;
+        int start = 0;
+        int end = rows * cols - 1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
             int row = mid / cols;
             int col = mid % cols;
@@ -65,9 +39,68 @@ class SearchIn2DMatrix {
             if (matrix[row][col] == target) {
                 return true;
             } else if (matrix[row][col] < target) {
-                low = mid + 1;
+                start = mid + 1;
             } else {
-                high = mid - 1;
+                end = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    private int search(int[] array, int target) {
+        int start = 0;
+        int end = array.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    int findRow(int[][] matrix, int target) {
+        int start = 0;
+        int end = matrix.length - 1;
+        int row = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (matrix[mid][0] == target) {
+                return mid;
+            } else if (matrix[mid][0] < target) {
+                row = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return row;
+    }
+
+    boolean searchInRow(int[] row, int target) {
+        int start = 0;
+        int end = row.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (row[mid] == target) {
+                return true;
+            } else if (row[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
 
