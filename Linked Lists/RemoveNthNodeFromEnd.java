a80e7ae5eed168e612cc7055adcfb43dae820172
mdtabrezhossain <142249992+mdtabrezhossain@users.noreply.github.com>
2026-07-13 22:28:18 +0530

    13-jul-26

--- Linked Lists/RemoveNthNodeFromEnd.java
+++ Linked Lists/RemoveNthNodeFromEnd.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+
 class RemoveNthNodeFromEnd {
+    int N;
+
     ListNode removeNthFromEnd(ListNode head, int n) {
         ArrayList<ListNode> nodes = new ArrayList<>();
 
@@ -15,6 +19,73 @@ class RemoveNthNodeFromEnd {
             return head.next;
 
         nodes.get(idx - 1).next = nodes.get(idx).next;
+
+        return head;
+    }
+
+    ListNode removeNthFromEnd2(ListNode head, int n) {
+        N = n;
+        return helper(head);
+    }
+
+    private ListNode helper(ListNode node) {
+        if (node == null)
+            return null;
+
+        node.next = helper(node.next);
+
+        N--;
+
+        if (N == 0)
+            return node.next;
+
+        return node;
+    }
+
+    ListNode removeNthFromEnd3(ListNode head, int n) {
+        if (head == null)
+            return null;
+
+        ListNode current = head;
+        int length = 0;
+
+        while (current != null) {
+            length++;
+            current = current.next;
+        }
+
+        int targetPos = length - n + 1;
+
+        if (targetPos == 1)
+            return head.next;
+
+        current = head;
+
+        for (int pos = 1; pos <= targetPos - 2; pos++) {
+            current = current.next;
+        }
+
+        current.next = current.next.next;
+
+        return head;
+    }
+
+    ListNode removeNthFromEnd4(ListNode head, int n) {
+        ListNode i = head;
+        ListNode j = i;
+
+        for (int k = 1; k <= n; k++)
+            j = j.next;
+
+        if (j == null)
+            return head.next;
+
+        while (j.next != null) {
+            i = i.next;
+            j = j.next;
+        }
+
+        i.next = i.next.next;
 
         return head;
     }
