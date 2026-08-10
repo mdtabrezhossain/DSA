2487c4e8c712d73ddc623b8d19f992dbaae54d05
mdtabrezhossain <142249992+mdtabrezhossain@users.noreply.github.com>
2026-08-10 21:20:53 +0530

    10-8-26

--- Trees/Validation/BalancedBinaryTree.java
+++ Trees/Validation/BalancedBinaryTree.java
@@ -18,4 +18,49 @@ class BalancedBinaryTree {
 
         return new int[] { isBalanced ? 1 : 0, height };
     }
+
+    boolean isBalanced2(TreeNode root) {
+        if (root == null)
+            return true;
+
+        Stack<Pair<TreeNode, Boolean>> stack = new Stack<>();
+        Map<TreeNode, int[]> map = new HashMap<>();
+
+        stack.push(new Pair<>(root, false));
+
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Boolean> entry = stack.pop();
+            TreeNode node = entry.getKey();
+            boolean visited = entry.getValue();
+
+            if (!visited) {
+                stack.push(new Pair<>(node, true));
+
+                if (node.left != null)
+                    stack.push(new Pair<>(node.left, false));
+
+                if (node.right != null)
+                    stack.push(new Pair<>(node.right, false));
+            } else {
+                int[] leftInfo = map.getOrDefault(node.left, new int[] { 1, 0 });
+                int[] rightInfo = map.getOrDefault(node.right, new int[] { 1, 0 });
+
+                boolean isLeftBalanced = leftInfo[0] == 1;
+                boolean isRightBalanced = rightInfo[0] == 1;
+                boolean areSubTreesBalanced = isLeftBalanced && isRightBalanced;
+
+                int leftHeight = leftInfo[1];
+                int rightHeight = rightInfo[1];
+                int height = 1 + Math.max(leftHeight, rightHeight);
+
+                boolean isBalanced = areSubTreesBalanced && (Math.abs(leftHeight - rightHeight) <= 1);
+
+                int[] info = { isBalanced ? 1 : 0, height };
+
+                map.put(node, info);
+            }
+        }
+
+        return map.get(root)[0] == 1;
+    }
 }
