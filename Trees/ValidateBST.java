8ea4442d0b9f3998ef75ef14bc93cd4b86be017d
mdtabrezhossain <142249992+mdtabrezhossain@users.noreply.github.com>
2026-08-16 23:57:12 +0530

    16-8-26

--- Trees/ValidateBST.java
+++ Trees/ValidateBST.java
@@ -1,25 +1,53 @@
-class ValidateBST {
-    boolean result = true;
-    Integer prevNodeVal = null;
+import java.util.LinkedList;
+import java.util.Queue;
 
+class ValidateBST {
     boolean isValidBST(TreeNode root) {
-        helper(root);
-        return result;
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    void helper(TreeNode node) {
-        if (node == null) {
-            return;
+    boolean helper(TreeNode root, long lowerBound, long upperBound) {
+        if (root == null)
+            return true;
+
+        if (lowerBound < root.val && root.val < upperBound)
+            return helper(root.left, lowerBound, root.val)
+                    && helper(root.right, root.val, upperBound);
+
+        return false;
+    }
+
+    boolean isValidBST2(TreeNode root) {
+        Queue<Pair<TreeNode, long[]>> queue = new LinkedList<>();
+
+        if (root != null)
+            queue.offer(new Pair<>(
+                    root,
+                    new long[] { Long.MIN_VALUE, Long.MAX_VALUE }));
+
+        while (!queue.isEmpty()) {
+            Pair<TreeNode, long[]> current = queue.poll();
+
+            TreeNode node = current.getKey();
+            long lowerBound = current.getValue()[0];
+            long upperBound = current.getValue()[1];
+
+            if (node.val <= lowerBound || node.val >= upperBound)
+                return false;
+
+            if (node.left != null)
+                queue.offer(
+                        new Pair<>(
+                                node.left,
+                                new long[] { lowerBound, node.val }));
+
+            if (node.right != null)
+                queue.offer(
+                        new Pair<>(
+                                node.right,
+                                new long[] { node.val, upperBound }));
         }
 
-        helper(node.left);
-
-        if (prevNodeVal != null && node.val <= prevNodeVal) {
-            result = false;
-            return;
-        }
-
-        prevNodeVal = node.val;
-        helper(node.right);
+        return true;
     }
 }
